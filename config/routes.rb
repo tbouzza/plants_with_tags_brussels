c20ad3b5466024317c9794fd2c89fb6ac70e14a6
Tarig Bouazzati <tbouzza@gmail.com>
2020-12-19 20:00:53 +0400

    added select 2

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [:create]
+  end
+
+  resources :plants, only: [ :destroy ] do
+    resources :plant_tags, only: [ :new, :create ]
+  end
+
+end
